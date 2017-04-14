ac32674577ebef097d1b4ddd8042880ca7533e93
Kelly Kashuda <klkashuda@crimson.ua.edu>
2017-04-14 03:22:12 +0000

    added style sheet and basic styling for layoout.erb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,12 @@
 class UsersController < ApplicationController
+  get '/login' do
+    if !logged_in?
+      erb :'/users/login'
+    else
+      redirect '/homepage'
+    end
+  end
 
-end 
+  
+
+end
